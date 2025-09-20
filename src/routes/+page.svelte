56ac489505e5174a6d5522bafd9713f611521976
AJ Caldwell <tomatrow@gmail.com>
2025-09-19 20:56:54 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	import Hero from './Hero.svelte';
+</script>
+
+<Header />
+<Hero />
---
+++ src/routes/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.svg';
+</script>
+
+<header>
+	<img src={logo} alt="Triathacore Logo" />
+</header>
+
+<style>
+</style>
