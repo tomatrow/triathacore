baa6abf30ae7e45a7f38e8ec8c784773d91d31a8
AJ Caldwell <tomatrow@gmail.com>
2025-09-22 20:14:10 -0700

    Add hero image text to cms

--- src/routes/HeroTag.svelte
+++ src/routes/HeroTag.svelte
@@ -1,7 +1,8 @@
-<h2>
-	<span>More Than a Race.</span>
-	<span>A Movement for Inclusion.</span>
-</h2>
+<script lang="ts">
+	import heroSection from "$lib/cms_data/hero-section.json"
+</script>
+
+<h2>{heroSection.imageSubtext}</h2>
 
 <style>
 	h2 {
@@ -11,11 +12,9 @@
 		font-size: 40px;
 		font-style: normal;
 		font-weight: 700;
-		line-height: 88%; /* 35.2px */
+		line-height: 40px;
+		white-space: pre;
 		letter-spacing: -0.2px;
-
-		display: flex;
-		flex-direction: column;
 		padding: 32px 20px;
 	}
 </style>
