78fcbcbcaa5a9fd8fd572f7b3545e29a2d7ece6f
AJ Caldwell <tomatrow@gmail.com>
2025-09-24 16:26:54 -0700

    Remove base

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ fallback: "404.html" }),
-		paths: { base: process.argv.includes("dev") ? "" : process.env.BASE_PATH }
+		adapter: adapter({ fallback: "404.html" })
 	}
 }
 
